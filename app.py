c0feaa5db6c4fca69acf0f3b98c52e3bdca44f1e
eythan <eythan092@gmail.com>
2021-07-07 17:18:16 +0300

    Added option to delete playlists from list and multiple bugfixes

--- app.py
+++ app.py
@@ -34,6 +34,7 @@ db.create_all()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    print(session)
     if request.method == 'POST':
         given_date = request.form['date']
         if given_date:
@@ -44,8 +45,9 @@ def index():
                     return redirect(url_for('index'))
             else:
                 playlists = list(session.get('playlists', {}).values())
+                print(playlists)
                 for playlist in playlists:
-                    if playlist.original_date == given_date:
+                    if playlist['original_date'] == given_date:
                         flash("This date already exists.")
                         return redirect(url_for('index'))
             uris = get_titles(given_date)
@@ -54,7 +56,8 @@ def index():
                 return redirect(url_for('index'))
             playlist_link = create_playlist(uris, given_date)
             if not current_user.is_authenticated:
-                session['playlists'] = {}
+                if 'playlists' not in session:
+                    session['playlists'] = {}
                 session['playlists'][given_date] = {'original_date': given_date, 'date': generate_date(given_date),
                                                     'link': playlist_link}
                 return redirect(url_for('index'))
@@ -62,12 +65,34 @@ def index():
                 return add_playlist(date=given_date, link=playlist_link)
         else:
             flash("Please enter a valid date.")
+            return redirect(url_for('index'))
     else:
         if not current_user.is_authenticated:
             playlists = list(session.get('playlists', {}).values())
         else:
             playlists = current_user.playlists
         return render_template('index.html', playlists=playlists)
+
+
+@app.route('/delete-playlist/<original_date>')
+def delete_playlist(original_date):
+    if current_user.is_authenticated:
+        requested_playlist = Playlist.query.filter_by(user=current_user, original_date=original_date).first()
+        if requested_playlist:
+            db.session.delete(requested_playlist)
+            db.session.commit()
+            return redirect(url_for('index'))
+        else:
+            flash("The requested playlist could not be found.")
+            return redirect(url_for('index'))
+    else:
+        print(session)
+        if original_date in session.get('playlists', {}):
+            session['playlists'].pop(original_date)
+            return redirect(url_for('index'))
+        else:
+            flash("The requested playlist could not be found.")
+            return redirect(url_for('index'))
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -104,4 +129,4 @@ def logout():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8050)
